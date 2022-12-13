037a34fad9ed02f2b83281a6796f80211c326527
Gurpreet <gs109690@gmail.com>
2022-12-12 18:44:35 -0600

    Crowdfund contract

--- contracts/KickStarter.sol
+++ contracts/KickStarter.sol
@@ -2,5 +2,21 @@
 pragma solidity ^0.8.15;
 
 contract KickStarter {
-    uint256 public projects;
+    uint256 public totalProjects;
+
+    struct Projects {
+        string projectName;
+        uint256 projectId;
+        uint256 fundGoal;
+        address payable creator;
+    }
+
+    mapping(address => uint256) public crowdEarning;
+
+    function createProject(
+        string memory _projectName,
+        uint256 _projectId,
+        uint256 _fundGoal,
+        address payable _creator
+    ) public {}
 }
