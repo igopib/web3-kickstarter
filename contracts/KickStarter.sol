cf7d4320586d1edb3fd3de9ed1d6901b5180a3b3
Gurpreet <gs109690@gmail.com>
2022-12-13 18:57:42 -0600

    Make project fn

--- contracts/KickStarter.sol
+++ contracts/KickStarter.sol
@@ -9,14 +9,25 @@ contract KickStarter {
         uint256 projectId;
         uint256 fundGoal;
         address payable creator;
+        bool finished;
     }
+
+    // Keeps track of projectId => data type Items
+    mapping(uint256 => Projects) public projects;
 
     mapping(address => uint256) public crowdEarning;
 
     function createProject(
         string memory _projectName,
-        uint256 _projectId,
-        uint256 _fundGoal,
-        address payable _creator
-    ) public {}
+        uint256 _fundGoal
+    ) public {
+        totalProjects++;
+        projects[totalProjects] = Projects(
+            _projectName,
+            totalProjects,
+            _fundGoal,
+            payable(msg.sender),
+            false
+        );
+    }
 }
