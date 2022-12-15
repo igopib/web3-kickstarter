4e4755d4a86ee4e8986ddb1a89c1d7bcec9909c3
Gurpreet <gs109690@gmail.com>
2022-12-14 22:32:33 -0600

    timer

--- contracts/KickStarter.sol
+++ contracts/KickStarter.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.15;
 
 contract KickStarter {
     uint256 public totalProjects;
+    uint256 public fundTimer;
 
     struct Projects {
         string projectName;
@@ -10,12 +11,17 @@ contract KickStarter {
         uint256 fundGoal;
         address payable creator;
         bool finished;
+        uint256 timeCreated;
     }
 
     // Keeps track of projectId => data type Items
     mapping(uint256 => Projects) public projects;
 
     mapping(address => uint256) public crowdEarning;
+
+    constructor() {
+        fundTimer = 30 days;
+    }
 
     function createProject(
         string memory _projectName,
@@ -27,7 +33,8 @@ contract KickStarter {
             totalProjects,
             _fundGoal,
             payable(msg.sender),
-            false
+            false,
+            block.timestamp
         );
     }
 }
