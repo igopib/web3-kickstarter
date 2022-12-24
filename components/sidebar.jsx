839f929f79f66b4e733e232b9220909d39c60f28
Gurpreet <gs109690@gmail.com>
2022-12-23 23:32:38 -0600

    sidebar layout

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -1,7 +1,11 @@
 import React, {useState} from 'react'
+import Link from 'next/link'
 
 export default function SideBar(){
+    const [isActive, setIsActive] = useState("dashboard")
   return (
-    <div>SideBar</div>
+    <div className='flex justify-between items-center flex-col sticky top-5 h-[93vh]'>
+      
+    </div>
   )
 }
