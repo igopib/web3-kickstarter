3e047f18f2dbd97d6e2ef879b96ecf563c9f0f42
Gurpreet <gs109690@gmail.com>
2022-12-28 23:49:08 -0600

    NAvigation links

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "713d12b05853d839"; }
+/******/ 		__webpack_require__.h = function() { return "1e059795e4722ad0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -26,7 +26,17 @@ export default function SideBar(){
       <div className='flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded=[20px] w-[76px] py-4 mt-12'>
         <div className="flex flex-col justify-center items-center gap-3">
             {navlinks.map((Link) => (
-              <Icon />
+              <Icon 
+              key={Link.name}
+              {...link}
+              isActive={isActive}
+              handleClick={() =>  {
+                if(!link.disabled) {
+                  setIsActive(link.name);
+                  navigator(link.link);
+                }
+              }}
+              />
             ))}
         </div>
       </div>
